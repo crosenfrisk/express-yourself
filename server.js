2c15c54064ac45d4139b62f847b5bc0b5a6850bb
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-02-26 19:13:45 -0600

    adding controllers/api-routes from just-tech-news, will modify

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const express = require('express');
-const session = require('session');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('.config/connection');
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -21,9 +21,9 @@ const sess = {
   
   app.use(session(sess));
   
-//   const helpers = require('./utils/helpers');
+  const helpers = require('./utils/helpers');
   
-//   const hbs = exphbs.create({ helpers });
+  const hbs = exphbs.create({ helpers });
   
   app.engine('handlebars', hbs.engine);
   app.set('view engine', 'handlebars');
@@ -32,7 +32,7 @@ const sess = {
   app.use(express.urlencoded({ extended: false }));
   app.use(express.static(path.join(__dirname, 'public')));
   
-//   app.use(require('./controllers/'));
+  app.use(require('./controllers/'));
   
   sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
