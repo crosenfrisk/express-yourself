d528ca8fd36b2faa021b5f0eadc59b9e6bad47de
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-03-02 17:20:36 -0600

    created force-logout.js and put in <script> on main.handlebars. Log out for inactivity works now!

---
+++ public/javascript/force-logout.js
@@ -0,0 +1,34 @@
+// Time out function for ALL pages; users must be active while logged in to continue to
+// make posts, comment, and edit posts/comments, delete posts/comments.
+let inactivityTime = function () {
+  let time;
+
+  window.onload = resetTimer;
+  document.onmousemove = resetTimer;
+  document.onkeydown = resetTimer;
+  document.onclick = resetTimer;
+
+  async function forcedLogout() {
+    alert("You have been logged out for inactivity, please login again!");
+    const response = await fetch("/api/users/logout", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      response.statusText;
+    }
+  }
+
+  function resetTimer() {
+    clearTimeout(time);
+    // logout after 30 min of inactivity
+    time = setTimeout(forcedLogout, 1800000);
+  }
+};
+
+window.onload = function () {
+  inactivityTime();
+};
