f3353883fdf868a62efe77d2d3ec6aecb16fef8a
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-03-06 14:17:03 -0600

    updated add-post.js comments

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,9 +1,13 @@
 async function createNewPost(event) {
+  // Prevent page from re-loading
   event.preventDefault();
 
+  // Get title name from title input field
   const title = document.querySelector('input[name="post-title"]').value;
+  // Get post content from post_content textarea
   const post_content = document.querySelector('textarea[name="post_content"]').value;
 
+  // Create new post using Post method to add title and post content to database using JSON.
   const response = await fetch(`/api/posts`, {
     method: 'POST',
     body: JSON.stringify({
@@ -15,6 +19,7 @@ async function createNewPost(event) {
     }
   });
 
+  // If all goes well, return to dashboard view, otherwise alert with status / error
   if (response.ok) {
     document.location.replace('/dashboard');
   } else {
@@ -22,4 +27,5 @@ async function createNewPost(event) {
   }
 }
 
+// Use button with class "new-post-form" to create a new post on click or enter
 document.querySelector('.new-post-form').addEventListener('submit', createNewPost);
