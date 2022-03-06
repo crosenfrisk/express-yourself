cfeda29579ea968b8d15c2be6e4947a8a508240f
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-03-06 14:19:05 -0600

    Updated comments for comment.js

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,11 +1,15 @@
 async function commentFormHandler(event) {
+  // Prevent page from re-loading
   event.preventDefault();
 
+  // Get comment text from textarea
   const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+  // Get post id from URL
   const post_id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
 
+  // Create new comment using Post method and submit to database using JSON
   if (comment_text) {
     const response = await fetch('/api/comments', {
       method: 'POST',
@@ -18,6 +22,7 @@ async function commentFormHandler(event) {
       }
     });
 
+    // If all goes well, reload page with new comment, otherwise alert error.
     if (response.ok) {
       document.location.reload();
     } else {
@@ -26,4 +31,5 @@ async function commentFormHandler(event) {
   }
 }
 
+// Use button "comment-form" to submit new comment on click or enter
 document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
