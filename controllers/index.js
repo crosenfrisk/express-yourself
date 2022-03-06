eb8b954296240122a8134fe048027937270f8517
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-03-06 14:07:47 -0600

    added one comment to controllers/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,7 @@ const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 
+// Tells server.js where to locate all routes
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
