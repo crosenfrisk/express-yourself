34c119740ecf71f26918285e3cf19e40600c686f
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-03-06 19:30:01 -0600

    Updated comments for login.js

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,9 +1,12 @@
 async function loginFormHandler(event) {
+  // Prevent page from re-loading
   event.preventDefault();
 
+  // Locate username and password from within login.handlebars 
   const username = document.querySelector('#username-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
+  // User must provide a username and a password to create login credentials using Post method/route, saving to db.
   if (username && password) {
     const response = await fetch('/api/users/login', {
       method: 'post',
@@ -14,6 +17,8 @@ async function loginFormHandler(event) {
       headers: { 'Content-Type': 'application/json' }
     });
 
+    // After login credentials are saved/created,
+    // user is directed to their dashboard where they can add new posts or edit/delete existing posts
     if (response.ok) {
       document.location.replace('/dashboard/');
     } else {
@@ -22,6 +27,7 @@ async function loginFormHandler(event) {
   }
 }
 
+// Login button, on click OR enter, will implement the login process with provided user credentials.
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
 
 
