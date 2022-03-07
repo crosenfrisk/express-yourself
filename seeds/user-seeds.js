f5ba4723834c258618f8196fe1dd4d80909bc119
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-03-06 19:47:08 -0600

    added comments to user-seeds.js

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,6 +1,8 @@
+// Import sequelize and User/Post models
 const sequelize = require('../config/connection');
 const { User, Post } = require('../models');
 
+// information to populate userdata seeds
 const userdata = [
   {
     username: 'camille0',
@@ -36,6 +38,8 @@ const userdata = [
   }
 ];
 
+// function to seed userdata, requiring hooks.
 const seedUsers = () => User.bulkCreate(userdata, {individualHooks: true});
 
+// export seedUsers
 module.exports = seedUsers;
