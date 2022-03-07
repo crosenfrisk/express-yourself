e3b214774c273969d911741aad9a3b829f07a592
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-03-06 19:49:53 -0600

    added comments to auth.js inside utils folder

--- utils/auth.js
+++ utils/auth.js
@@ -1,3 +1,10 @@
+// function to provide additional security to site, requiring users to be logged in
+// to make changes to any posts; can only edit and/or delete THEIR posts/comments 
+// tied to their user_id.
+
+// withAuth is required (as an import) for several routes; 
+// if the user isn't logged in, an error will occur.
+
 const withAuth = (req, res, next) => {
   if (!req.session.user_id) {
     res.redirect('/login');
@@ -6,4 +13,5 @@ const withAuth = (req, res, next) => {
   }
 };
 
+// To export withAuth function and use across application.
 module.exports = withAuth;
