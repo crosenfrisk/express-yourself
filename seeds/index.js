e6810b04248f682684c32035d9c60f71c2a76cc9
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-03-06 19:44:38 -0600

    Added descriptive comments to index.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,12 @@
+// Import all seed files
 const seedUsers = require('./user-seeds');
 const seedPosts = require('./post-seeds');
 const seedComments = require('./comment-seeds');
 
+// Import sequelize
 const sequelize = require('../config/connection');
 
+// function to run all seeds and populate the database
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('--------------');
@@ -19,4 +22,5 @@ const seedAll = async () => {
   process.exit(0);
 };
 
+// calling function, so when we run index.js, seeds are populated.
 seedAll();
