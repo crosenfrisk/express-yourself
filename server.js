b46ad12de1cf47fee6a2a2525d884174e03f2db4
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-03-06 20:24:22 -0600

    added comments to server.js

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+// Imports for server.js
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
+// To use express
 const app = express();
+// To display app on 3001 or wherever the app is deployed via Heroku
 const PORT = process.env.PORT || 3001;
 
+// Use sequelize to run configuration -- via config/connection and subsequent JAWS.DB extension
+// for .env variables
 const sequelize = require('./config/connection');
+// To create session for user
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -19,12 +25,16 @@ const sess = {
     })
   };
   
+  // to use session
   app.use(session(sess));
   
+  // importing helpers
   const helpers = require('./utils/helpers');
   
+  // using helpers
   const hbs = exphbs.create({ helpers });
   
+  // using handlebars for site
   app.engine('handlebars', hbs.engine);
   app.set('view engine', 'handlebars');
   
@@ -32,8 +42,10 @@ const sess = {
   app.use(express.urlencoded({ extended: false }));
   app.use(express.static(path.join(__dirname, 'public')));
   
+  // use routes
   app.use(require('./controllers/'));
   
+  // synce database using sequelize (rather than running MySQL every time)
   sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
   });
