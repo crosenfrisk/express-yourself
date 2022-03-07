8cd413568be18c9b14d72d52b58e462ba8a68a0d
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-03-06 19:19:25 -0600

    comments added to edit-post.js

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,13 +1,18 @@
 async function editFormHandler(event) {
+  // Prevent page from automatically reloading
   event.preventDefault();
 
+  // Locate title content from input field
   const title = document.querySelector('input[name="post-title"]').value.trim();
+  // Locate post content from textarea
   const post_content = document.querySelector('textarea[name="post_content"]').value.trim();
   
+  // Identify id of post to edit using split method to end of URL
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
   
+  // Update post using Put route, saving changes to title and / or post content.
   const response = await fetch(`/api/posts/${id}`, {
     method: 'PUT',
     body: JSON.stringify({
@@ -19,11 +24,14 @@ async function editFormHandler(event) {
     }
   });
 
+  // After update, return user to dashboard
   if (response.ok) {
     document.location.replace('/dashboard/');
+    // otherwise alert with error
   } else {
     alert(response.statusText);
   }
 }
 
+// On submit, edited post will be updated / saved to DB. 
 document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
