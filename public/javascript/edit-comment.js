dcf0ca646d4d99c844c6a5a44fb704481db1d90d
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-03-06 19:09:55 -0600

    Comments added to edit-comment.js

--- public/javascript/edit-comment.js
+++ public/javascript/edit-comment.js
@@ -1,12 +1,15 @@
 async function editCommentHandler(event) {
+  // Prevent page from reloading
 
     event.preventDefault();
   
+    // Identify the comment to edit by taking the id from the end of URL string
     const comment_text = document.querySelector('textarea[name="comment-text"]').value.trim();
     const id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
     
+    // Use PUT route to update comment (comment_text) for selected comment using id
     const response = await fetch(`/api/comments/${id}`, {
       method: 'PUT',
       body: JSON.stringify({
@@ -17,13 +20,15 @@ async function editCommentHandler(event) {
       }
     });
   
+    // Once comment is updated, return the user to the dashboard
     if (response.ok) {
       document.location.replace('/dashboard/');
     } else {
+      // otherwise alert with error 
       alert(response.statusText);
     }
   }
   
-
+  // Use "Save Comment" button to update edited comment on click
   document.querySelector('.save-comment').addEventListener('click', editCommentHandler);
   
