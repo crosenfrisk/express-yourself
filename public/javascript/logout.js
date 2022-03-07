abb7624e240c962dfc11b13742831960efd6f159
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-03-06 19:33:41 -0600

    Added comments to logout.js

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,14 +1,19 @@
 async function logout() {
+  // Function for logging user out using post method/route.
   const response = await fetch('/api/users/logout', {
     method: 'post',
     headers: { 'Content-Type': 'application/json' }
   });
 
+  // After being logged out, user is redirected to homepage, 
+  // removing access to posting, editiing, or deleting posts or comments.
   if (response.ok) {
     document.location.replace('/');
+    // if logout does not work, display alert/error
   } else {
     alert(response.statusText);
   }
 }
 
+// On clicking logout button, user is logged out from session.
 document.querySelector('#logout').addEventListener('click', logout);
